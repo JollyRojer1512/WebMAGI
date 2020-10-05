e18ed38bb8afadba70a070494a297f6b7e1e338d
JollyRojer1512 <58805641+JollyRojer1512@users.noreply.github.com>
2020-10-05 14:34:11 +0500

    order-changed
    changed order to another look and remain it with display style

--- js/script.js
+++ js/script.js
@@ -3,24 +3,24 @@ window.onload = function(){
     const background = document.getElementsByTagName("background");
     const order = document.getElementsByClassName("order");
     const box = document.getElementsByClassName("ourcontacts");
-    const container = document.getElementsByClassName("ourcontacts_container");
-    const change = document.getElementsByClassName("change");
+    // const container = document.getElementsByClassName("ourcontacts_container");
+    // const change = document.getElementsByClassName("change");
 
     for(let i = 0; i < order.length; i++){
         order[i].addEventListener('click', function(){
             box[0].classList.toggle('inactive');
             background[0].classList.toggle('active');
-            container[0].classList.toggle('inactive');
-            change[0].addEventListener('click', toggleWorkers);
-            change[1].addEventListener('click', toggleWorkers);
+            // container[0].classList.toggle('inactive');
+            // change[0].addEventListener('click', toggleWorkers);
+            // change[1].addEventListener('click', toggleWorkers);
         });
     }
     
     background[0].addEventListener('click', function(){
         box[0].classList.toggle('inactive');
         background[0].classList.toggle('active');
-        container[0].classList.add('inactive');
-        container[1].classList.add('inactive');
+        // container[0].classList.add('inactive');
+        // container[1].classList.add('inactive');
     });
     
     // if(!box[0].classList.contains('inactive'))
@@ -28,9 +28,9 @@ window.onload = function(){
     //         box[0].classList.add('inactive');
     //     });
 
-    function toggleWorkers(){
-        container[0].classList.toggle('inactive');
-        container[1].classList.toggle('inactive');
-    }
+    // function toggleWorkers(){
+    //     container[0].classList.toggle('inactive');
+    //     container[1].classList.toggle('inactive');
+    // }
 
 };
